d342fb7abb998c01c56d227cf1c7ce34d19688ab
Luke Oliff <luke@lukeoliff.com>
2020-08-08 06:03:36 +0100

    remove client specific code

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var app = express();
+var corsOpt = {
+  origin: process.env.CLIENT_URL,
+  optionsSuccessStatus: 200
+};
 
+app.use(cors(corsOpt))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.use('/api/server', require('./routes/server'));
 app.use('/api/user', require('./routes/user'));
 app.use('/api/member', require('./routes/member'));
-
-app.use(express.static(path.join(__dirname, 'client/dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/dist/index.html'))
-});
 
 module.exports = app;
