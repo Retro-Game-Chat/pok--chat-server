a906120152795f3582b846d8db423eaa9f9dc3ff
Luke Oliff <luke@lukeoliff.com>
2020-08-10 10:01:51 +0100

    remove cors origin restriction

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 var express = require('express');
-var path = require('path');
 var logger = require('morgan');
 var cors = require('cors');
 
 var app = express();
-var corsOpt = {
-  origin: process.env.CLIENT_URL,
-  optionsSuccessStatus: 200
-};
 
-app.use(cors(corsOpt))
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
